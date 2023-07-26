933b6c1f55e20f9e7080a97b20818b209d89c465
MR STRATEGIST <140485849+tandavbot@users.noreply.github.com>
2023-07-26 22:45:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-   TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-    API_ID = int(os.environ.get("APP_ID", 12345))
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH", "")
-   UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
