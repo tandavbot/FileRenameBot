a42b612877498f64245188f63fa0a3889234823d
MR STRATEGIST <140485849+tandavbot@users.noreply.github.com>
2023-07-26 22:36:15 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config(object):
+   TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    API_ID = int(os.environ.get("APP_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
+   UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
